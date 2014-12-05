570cfec5cf9065c611c12bc9ae0cd5e60658c743
oncomouse <oncomouse@gmail.com>
2014-12-04 17:22:44 -0700

    cssmin support.

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,12 +13,18 @@ module.exports = function(grunt) {
 		},
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> v<%= pkg.version %> (also contains jquery-overlaps v 1.2.3) |  <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+				banner: '/*! <%= pkg.name %> v<%= pkg.version %> (also contains jquery-overlaps v 1.2.3) |	<%= grunt.template.today("dd-mm-yyyy") %> */\n'
 			},
 			dist: {
 				src: 'dist/jquery.inline-footnotes.js',
 				dest: 'dist/jquery.inline-footnotes.min.js'
 	 		}
+		},
+		cssmin: {
+			dist: {
+				src: 'stylesheets/jquery.inline-footnotes.css',
+				dest: 'dist/jquery.inline-footnotes.min.css'
+			}
 		},
 		clean: {
 			js: ["dist/*.js", "!dist/*.min.js"]
@@ -42,8 +48,9 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-bump');
 
-	grunt.registerTask('default', ['concat', 'uglify', 'clean']);
+	grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'clean']);
 
 };
