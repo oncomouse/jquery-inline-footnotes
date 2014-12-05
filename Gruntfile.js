dcd58518edfde937232360778e6bede252cc7c3d
oncomouse <oncomouse@gmail.com>
2014-12-04 17:30:04 -0700

    Why anyone would do this is beyond me

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,6 +41,7 @@ module.exports = function(grunt) {
 		bump: {
 			options: {
 				files: ['package.json', 'bower.json'],
+				commitFiles: ['package.json', 'bower.json'],
 				 pushTo: 'origin'
 			}
 		}
